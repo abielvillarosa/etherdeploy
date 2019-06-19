258ad3c6f5771d6aba13b08ddeb3b9a847250af9
Abiel Villarosa <john_abiel@yahoo.com>
2019-06-19 15:50:20 -0400

    add funct call and events

--- deploy_contract.js
+++ deploy_contract.js
@@ -11,15 +11,36 @@ let provider = ethers.getDefaultProvider('ropsten');
 // console.log(process.env.pk);
 
 let wallet = new ethers.Wallet(process.env.pk, provider);
+let posterwallet = new ethers.Wallet(process.env.pk2, provider);
 // console.log(wallet);
 
 //Deploy contract
-let ContractFactory = new ethers.ContractFactory(ABI, bytecode, wallet);
+// let ContractFactory = new ethers.ContractFactory(ABI, bytecode, wallet);
 
-async function deployContract() {
-    let contract = await ContractFactory.deploy();
-    await contract.deployed();
-    console.log(contract);
+// async function deployContract() {
+//     let contract = await ContractFactory.deploy();
+//     await contract.deployed();
+//     console.log(contract);
+// }
+
+// deployContract();
+
+//Interacting with Contract
+let address = "0x2Ec304726Db5BE65F46E3Bd9549b5dd4db44Ed66";
+let contract = new ethers.Contract( address, ABI, wallet).connect(provider);
+// console.log(contract);
+
+let workingContract = contract.connect(posterwallet);
+
+//Call functions
+async function postBooking() {
+    let tx = await workingContract.postBooking("Westjet", 20190801, 1000);
+    console.log("get value: ", tx);
 }
+postBooking();
 
-deployContract();
+//Events
+workingContract.on("swapBookingEvent", (poster, to) => {
+    console.log("poster: ", poster);
+    console.log("swopper: ", to);
+})
